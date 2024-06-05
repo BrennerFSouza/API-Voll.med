f512244dad84b6a09dbc01143bbf12d13629a591
brenner.souza <brenner.souza@gmail.com>
2024-06-04 23:16:47 -0300

    Concluido cadastro e listagem de medicos

--- src/main/java/med/voll/apivollmed/controller/MedicoController.java
+++ src/main/java/med/voll/apivollmed/controller/MedicoController.java
@@ -1,14 +1,36 @@
 package med.voll.apivollmed.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.apivollmed.medico.DadosCadastroMedico;
+import med.voll.apivollmed.medico.DadosListagemMedico;
+import med.voll.apivollmed.medico.Medico;
+import med.voll.apivollmed.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
-@RequestMapping("/medicos")
+@RequestMapping("medicos")
 public class MedicoController {
+    @Autowired
+    private MedicoRepository repository;
 
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
     }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+        return repository.findAll(paginacao)
+                .map(DadosListagemMedico::new);
+    }
+
+
 }
---
+++ src/main/java/med/voll/apivollmed/endereco/Endereco.java
@@ -0,0 +1,32 @@
+package med.voll.apivollmed.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numero;
+
+    public Endereco(DadosEndereco endereco) {
+        this.logradouro = endereco.logradouro();
+        this.cep = endereco.cep();
+        this.uf = endereco.uf();
+        this.bairro = endereco.bairro();
+        this.cidade = endereco.cidade();
+        this.complemento = endereco.complemento();
+        this.numero = endereco.numero();
+    }
+}
